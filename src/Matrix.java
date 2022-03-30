6554024e69b94c7aac4f9a78dfcb94d1ba2591fe
Ryan Trinh <rqtrinh@cpp.edu>
2022-03-30 16:37:43 -0700

    Finished divide and conquer method, fixed some array out of bounds bugs, improved subtraction/addition methods

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,8 +1,24 @@
+//Import necessary libraries
+import java.util.Arrays;
+
 public class Matrix {
-  public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception 
+    {
+        Matrix matrix =  new Matrix();
+        int [][] A = {{1,1,1,1},
+                      {1,1,1,1},
+                      {1,1,1,1},
+                      {1,1,1,1}};
+        int [][] C =  matrix.strassen(4, A, A);
+        for (int [] row : C)
+        {
+            System.out.println(Arrays.toString(row));
+        }
+        System.out.println(A);
     }
 
+
+    //Traditional Matrix Multiplication
     public int[][] traditional(int n, int [][] A, int [][] B)
     {
         int [][] C = new int[n][n];
@@ -17,6 +33,48 @@ public class Matrix {
             }
         }
         return C;
+    }
+
+    //Divide and conquer approach
+    public int[][] divideConquer(int n, int [][] A, int [][] B)
+    {
+        //base case for divide and conquer approach
+        if(n == 2) 
+        {
+            int c11 = A[0][0]*B[0][0] + A[0][1]*B[1][0];
+            int c12 = A[0][0]*B[0][1] + A[0][1]*B[1][1];
+            int c21 = A[1][0]*B[0][0] + A[1][1]*B[1][0];
+            int c22 = A[1][0]*B[0][1] + A[1][1]*B[1][1];
+
+            //create return matrix
+            int [][] C = { {c11, c12},
+                           {c21, c22} };
+            
+            return C;
+        }    
+
+        else
+        {
+            //Partition A matrix
+            int [][] A11 = partition(n, 11, A);
+            int [][] A12 = partition(n, 12, A);
+            int [][] A21 = partition(n, 21, A);
+            int [][] A22 = partition(n, 22, A);
+
+            //Partition B matrix
+            int [][] B11 = partition(n, 11, B);
+            int [][] B12 = partition(n, 12, B);
+            int [][] B21 = partition(n, 21, B);
+            int [][] B22 = partition(n, 22, B);
+
+            int [][] C11 = addition(divideConquer(n/2, A11, A12), divideConquer(n/2, A12, B21));
+            int [][] C12 = addition(divideConquer(n/2, A11, B12), divideConquer(n/2, A12, B22));
+            int [][] C21 = addition(divideConquer(n/2, A21, B11), divideConquer(n/2, A22, B21));
+            int [][] C22 = addition(divideConquer(n/2, A21, B12), divideConquer(n/2, A22, B22));
+
+            int [][] C = repartition(n, C11, C12, C21, C22);
+            return C;
+        }
     }
 
     public int[][] strassen(int n, int [][] A, int [][] B)
@@ -52,19 +110,19 @@ public class Matrix {
             int [][] B22 = partition(n, 22, B);
 
             //Recursivley call strassen
-            int [][] P = strassen(n/2, addition(n, A11, A22), addition(n, B11, B22));
-            int [][] Q = strassen(n/2, addition(n, A21, A22), B11);
-            int [][] R = strassen(n/2, A11, subtraction(n, B12, B22));
-            int [][] S = strassen(n/2, A22, subtraction(n, B21, B11));
-            int [][] T = strassen(n/2, addition(n, A11, A12), B22);
-            int [][] U = strassen(n/2, subtraction(n, A21, A11), addition(n, B11, B12));
-            int [][] V = strassen(n/2, subtraction(n, A21, A22), addition(n, B11, B12));
+            int [][] P = strassen(n/2, addition(A11, A22), addition(B11, B22));
+            int [][] Q = strassen(n/2, addition(A21, A22), B11);
+            int [][] R = strassen(n/2, A11, subtraction(B12, B22));
+            int [][] S = strassen(n/2, A22, subtraction(B21, B11));
+            int [][] T = strassen(n/2, addition(A11, A12), B22);
+            int [][] U = strassen(n/2, subtraction(A21, A11), addition(B11, B12));
+            int [][] V = strassen(n/2, subtraction(A21, A22), addition(B11, B12));
 
             //Calculate C11, C12, C21, C22 from our recursive calls
-            int [][] C11 = addition(n, subtraction(n, addition(n, P, S), T), V);
-            int [][] C12 = addition(n, R, T);            
-            int [][] C21 = addition(n, Q, S);
-            int [][] C22 = addition(n, subtraction(n, addition(n, P, R), Q), U);
+            int [][] C11 = addition(subtraction(addition(P, S), T), V);
+            int [][] C12 = addition(R, T);            
+            int [][] C21 = addition(Q, S);
+            int [][] C22 = addition(subtraction(addition(P, R), Q), U);
 
             //Create C matrix from its qudrants and return complete matrix
             int [][] C = repartition(n, C11, C12, C21, C22);
@@ -75,12 +133,12 @@ public class Matrix {
     }
 
     //Matrix addition for matrix of matching width and height
-    public int[][] addition(int n, int[][] A, int[][] B)
+    public int[][] addition(int[][] A, int[][] B)
     {
-        int [][] C = new int[n][n];
-        for(int i = 0; i < n; i++)
+        int [][] C = new int[A.length][A[0].length];
+        for(int i = 0; i < A.length; i++)
         {
-            for(int j = 0; j < n; j++)
+            for(int j = 0; j < A[0].length; j++)
             {
                 C[i][j] = A[i][j] + B[i][j];
             }
@@ -89,12 +147,12 @@ public class Matrix {
     }
 
      //Matrix subtraction for matrix of matching width and height
-    public int[][] subtraction(int n, int[][] A, int[][] B)
+    public int[][] subtraction(int[][] A, int[][] B)
     {
-        int [][] C = new int[n][n];
-        for(int i = 0; i < n; i++)
+        int [][] C = new int[A.length][A[0].length];
+        for(int i = 0; i < A.length; i++)
         {
-            for(int j = 0; j < n; j++)
+            for(int j = 0; j < A[0].length; j++)
             {
                 C[i][j] = A[i][j] - B[i][j];
             }
@@ -108,38 +166,38 @@ public class Matrix {
         switch (part) 
         {
             case 11:
-                for(int i = 0; i < n/2; i++)
+                for(int i = 0; i < (n/2); i++)
                 {
-                    for(int j = 0; j < n/2; j++)
+                    for(int j = 0; j < (n/2); j++)
                     {
                         quadrant[i][j] = matrix[i][j];
                     }
                 }
                 break;
             case 12:
-                for(int i = 2; i < n/2; i++)
+                for(int i = 0; i < (n/2); i++)
                 {
-                    for(int j = 0; j < (n/2)+2; j++)
+                    for(int j = 0; j < (n/2); j++)
                     {
-                        quadrant[i][j] = matrix[i][j];
+                        quadrant[i][j] = matrix[i][j+quadrant[0].length];
                     }
                 }
                 break;
             case 21:
-                for(int i = 0; i < (n/2)+2; i++)
+                for(int i = 0; i < (n/2); i++)
                 {
-                    for(int j = 0; j < n/2; j++)
+                    for(int j = 0; j < (n/2); j++)
                     {
-                        quadrant[i][j] = matrix[i][j];
+                        quadrant[i][j] = matrix[i+quadrant.length][j];
                     }
                 }                
                 break;
             case 22:
-                for(int i = 0; i < (n/2)+2; i++)
+                for(int i = 0; i < (n/2); i++)
                 {
-                    for(int j = 0; j < (n/2)+2; j++)
+                    for(int j = 0; j < (n/2); j++)
                     {
-                        quadrant[i][j] = matrix[i][j];
+                        quadrant[i][j] = matrix[i+quadrant.length][j+quadrant[0].length];
                     }
                 }
                 break;
@@ -150,36 +208,36 @@ public class Matrix {
     public int [][] repartition(int n, int [][] C11, int [][] C12, int [][] C21, int [][] C22 )
     {
         //initialize return matrix
-        int [][] complete = new int[n*2][n*2];
+        int [][] complete = new int[n][n];
 
         //repartition C11
-        for(int i = 0; i < n; i++)
+        for(int i = 0; i < n/2; i++)
         {
-            for(int j = 0; j < n; j++)
+            for(int j = 0; j < n/2; j++)
             {
                 complete[i][j] = C11[i][j];
             }
         }
         //repartition C12
-        for(int i = 0; i < n; i++)
+        for(int i = 0; i < n/2; i++)
         {
-            for(int j = 0; j < n; j++)
+            for(int j = 0; j < n/2; j++)
             {
                 complete[i+2][j] = C11[i][j];
             }
         }
         //repartition C21
-        for(int i = 0; i < n; i++)
+        for(int i = 0; i < n/2; i++)
         {
-            for(int j = 0; j < n; j++)
+            for(int j = 0; j < n/2; j++)
             {
                 complete[i][j+2] = C21[i][j];
             }
         }
         //repartition C22
-        for(int i = 0; i < n; i++)
+        for(int i = 0; i < n/2; i++)
         {
-            for(int j = 0; j < n; j++)
+            for(int j = 0; j < n/2; j++)
             {
                 complete[i+2][j+2] = C22[i][j];
             }
